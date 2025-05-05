8ca4251f0fc525bbcda35c520b518da60f7608ac
jonas5 <jo@live.no>
2025-05-05 19:23:18 +0100

    Update index.js
    revert changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const core = require('@actions/core');
 const version = process.argv[2]; // Получение версии OpenWRT из аргумента командной строки
 
 const SNAPSHOT_TARGETS_TO_BUILD = ['mediatek', 'ramips', 'x86', 'armsr'];
-const SNAPSHOT_SUBTARGETS_TO_BUILD = ['filogic', 'mt7622', 'mt7623', 'mt7629', 'mt7620', 'mt7621', 'mt76x8', '64', 'generic', 'armv8', 'mt7988a'];
+const SNAPSHOT_SUBTARGETS_TO_BUILD = ['filogic', 'mt7622', 'mt7623', 'mt7629', 'mt7620', 'mt7621', 'mt76x8', '64', 'generic', 'armv8'];
 
 if (!version) {
   core.setFailed('Version argument is required');
@@ -38,12 +38,7 @@ async function getTargets() {
 
 async function getSubtargets(target) {
   const $ = await fetchHTML(`${url}${target}/`);
-  const subtargets = [];
-
-  if (target === 'mediatek') {
-    return ['mt7988a'];
-  }
-  
+  const subtargets = [];  
   $('table tr td.n a').each((index, element) => {
     const name = $(element).attr('href');
     if (name && name.endsWith('/')) {
